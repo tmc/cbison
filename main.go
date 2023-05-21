e4e60a4cbc2876a4e4cb6512f7f373616bb8923b
Travis Cline <travis.cline@gmail.com>
2023-05-20 18:57:37 -0700

    cbison: Get working

--- main.go
+++ main.go
@@ -4,7 +4,12 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"os"
+
+	"golang.org/x/oauth2"
+	"google.golang.org/api/option"
+	"google.golang.org/grpc/metadata"
 
 	generativelanguagepb "google.golang.org/genproto/googleapis/ai/generativelanguage/v1beta2"
 	generativelanguage "google.golang.org/google/ai/generativelanguage/v1beta2"
@@ -20,27 +25,49 @@ func main() {
 
 func run() error {
 	ctx := context.Background()
-	c, err := generativelanguage.NewDiscussClient(ctx)
+	ctx = metadata.AppendToOutgoingContext(ctx, "x-goog-api-key", os.Getenv("GOOGLE_GENERATIVE_AI_API_KEY"))
+	dc, err := generativelanguage.NewDiscussClient(ctx, option.WithTokenSource(oauth2.StaticTokenSource(&oauth2.Token{})))
 	if err != nil {
 		return err
 	}
-	defer c.Close()
+	defer dc.Close()
+
+	in, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		return err
+	}
 
 	req := &generativelanguagepb.GenerateMessageRequest{
-		Model: "name=chat-bison@001",
+		Model: "models/chat-bison-001",
 		Prompt: &generativelanguagepb.MessagePrompt{
 			Messages: []*generativelanguagepb.Message{{
-				Author:  "travis",
-				Content: "hello!",
+				Author:  "user",
+				Content: string(in),
 			},
 			},
 		},
 	}
-	resp, err := c.GenerateMessage(ctx, req)
+	resp, err := dc.GenerateMessage(ctx, req)
 	if err != nil {
 		return err
 	}
 	_ = resp
 	fmt.Println(resp)
+	return nil
+}
+
+func listModels(ctx context.Context) error {
+	c, err := generativelanguage.NewModelClient(ctx, option.WithTokenSource(oauth2.StaticTokenSource(&oauth2.Token{})))
+	if err != nil {
+		return err
+	}
+	mIter := c.ListModels(ctx, &generativelanguagepb.ListModelsRequest{})
+	for {
+		m, err := mIter.Next()
+		fmt.Println(m, err)
+		if err != nil {
+			break
+		}
+	}
 	return nil
 }
